c6d6627ba89198175c095b5e3a8f02696d163da4
Michał <michalgce@gmail.com>
2017-10-13 15:10:30 +0200

    Lets begin.

--- booking/src/main/java/pl/xsolve/booking/repositories/BookingRepo.java
+++ booking/src/main/java/pl/xsolve/booking/repositories/BookingRepo.java
@@ -1,10 +1,1 @@
-package pl.xsolve.booking.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import pl.xsolve.booking.entities.Booking;
-
-@Repository
-public interface BookingRepo extends CrudRepository<Booking, Long> {
-
-}
+// TODO :)
